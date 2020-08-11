3da005a08c8c87bf3cf30b032a1b375aa7854e6e
sierus <bcloudintelekt@gmail.com>
2020-08-11 09:25:27 +0500

    added account statement form img

--- account_statement/urls.py
+++ account_statement/urls.py
@@ -5,8 +5,9 @@ from .views import *
 app_name = 'account_statement'
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('new-car/', new_car, name='new_car'),
+    path('new-car/add-photo/<int:id>/', new_car_add_photo, name='new_car_add_photo'),
     path('word/', word, name='word'),
     path('get-car-type/', get_car_type, name='get_car_type'),
 
