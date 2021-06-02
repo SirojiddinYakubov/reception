8de908d468c85ec3e8270d42fbd8a4c06186a39c
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2021-06-02 17:18:09 +0500

    fix

--- click/models.py
+++ click/models.py
@@ -13,5 +13,5 @@ class Order(models.Model):
     created_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     is_paid = models.BooleanField(verbose_name='To\'langanligi',default=False)
     type = models.CharField(max_length=10,choices=PAYMENT_SERVICES_CHOICES,default=1)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True, default=1)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True,blank=True)
     amount = models.CharField(max_length=20,verbose_name="Summasi")
