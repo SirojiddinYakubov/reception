0c20c72c671b5f6e7a2575bdcd69703415c37215
Sirojiddin Yakubov <yakubov9791999@gmail.com>
2021-11-12 13:19:19 +0500

    application send to section button

--- static/js/main.js
+++ static/js/main.js
@@ -1270,3 +1270,15 @@ function get_sections(url) {
         })
     })
 }
+
+function htmlConvertToPlain(html){
+
+    // Create a new div element
+    var tempDivElement = document.createElement("div");
+
+    // Set the HTML content with the given value
+    tempDivElement.innerHTML = html;
+
+    // Retrieve the text property of the element
+    return tempDivElement.textContent || tempDivElement.innerText || "";
+}
