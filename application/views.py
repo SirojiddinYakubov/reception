412204670c47376f7a6f2843dbe7a15d4fe56cfa
yakubov <yakubov9791999@gmail.com>
2021-12-15 23:14:25 +0500

    add document polices

--- application/views.py
+++ application/views.py
@@ -380,11 +380,9 @@ class ConfirmApplicationData(APIView, AllowedRolesMixin):
                         application.confirmed_date = timezone.now()
                         application.save()
 
-                        document_polices = DocumentForPolice.objects.filter(is_active=True, service=application.service)
-
                         # text = f"Hurmatli foydalanuvchi! {application.id}-raqamli arizangiz muvvaffaqiyatli tasdiqlandi! {car.given_technical_passport} seriya va raqamli qayd etish guvohnomasi{' va {0} davlat raqam belgisini'.format(car.given_number) if car.given_number else 'ni'} {application.given_date.strftime('%d.%m.%Y') + '-yil'} {request.POST.get('given_time')} da {request.user.section.region.title} {request.user.section.title} ga kelib olib ketishingizni so'raymiz. Qo'shimcha ma'lumot uchun tel:972800809"
                         # text = f"Hurmatli foydalanuvchi! {application.id}-raqamli arizangiz muvvaffaqiyatli tasdiqlandi! {application.given_date.strftime('%d.%m.%Y') + '-yil'} {request.POST.get('given_time')} da {request.user.section.region.title} {request.user.section.title} ga kelib olib ketishingizni so'raymiz. Qo'shimcha ma'lumot uchun tel:972800809"
-                        text = f"E-RIB.UZ: {application.id}-raqamli arizangiz muvvaffaqiyatli tasdiqlandi! {application.section.title} ga kelib qayd etish guvohnomasini olib ketishingizni so'raymiz. {', '.join([d.title for d in document_polices])} Qo'shimcha ma'lumot uchun tel:972800809"
+                        text = f"E-RIB.UZ: {application.id}-raqamli arizangiz muvvaffaqiyatli tasdiqlandi! {application.section.title} ga kelib qayd etish guvohnomasini olib ketishingizni so'raymiz. Qo'shimcha ma'lumot uchun tel:972800809"
 
                         # create notification
                         notification = Notification.objects.create(application=application, sender=request.user,
@@ -426,7 +424,9 @@ class ConfirmApplicationData(APIView, AllowedRolesMixin):
                         application.inspector = request.user
                         application.save()
 
-                        text = f"E-RIB.UZ: {application.id}-raqamli arizangiz {application.section.title} tomonidan ko'rib chiqish uchun qabul qilindi! Quydagilarni {application.section.title} ga keltirib topshirishingizni so'raymiz!  Qo'shimcha ma'lumot uchun tel:972800809"
+                        document_polices = DocumentForPolice.objects.filter(is_active=True, service=application.service)
+
+                        text = f"E-RIB.UZ: {application.id}-raqamli arizangiz {application.section.title} tomonidan ko'rib chiqish uchun qabul qilindi! Quydagilarni {application.section.title} ga keltirib topshirishingizni so'raymiz! {', '.join([d.title for d in document_polices])} Qo'shimcha ma'lumot uchun tel:972800809"
 
                         # create notification
                         notification = Notification.objects.create(application=application, sender=request.user,
