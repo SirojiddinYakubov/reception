0844e83d86c751475d6dee5028dd03fc03a8e1e4
yakubov <yakubov9791999@gmail.com>
2021-12-20 23:00:08 +0500

    install vue js , diagnostics list worked

--- api/v1/partners/views.py
+++ api/v1/partners/views.py
@@ -2,14 +2,15 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from api.v1.partners import serializers
 from partners.models import (DiagnosticDepartment)
 
-class DistrictDiagnosticsList(APIView):
+class DistrictDiagnosticsList(generics.ListAPIView):
     queryset = DiagnosticDepartment.objects.filter(is_active=True)
     serializer_class = serializers.DistrictDiagnosticsListSerializer
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        return self.queryset.filter(region_id=self.kwargs.get('pk'))
+        return self.queryset.filter(district_id=self.kwargs.get('pk'))
