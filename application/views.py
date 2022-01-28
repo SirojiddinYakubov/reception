c4b2e9d70a8b5c03c64553b964b87552bfa8ebb7
yakubov <yakubov9791999@gmail.com>
2022-01-28 09:19:32 +0500

    some changes

--- application/views.py
+++ application/views.py
@@ -271,7 +271,7 @@ def create_application_doc(request, filename):
                    car=car,
                    made_year=car.made_year.strftime("%d.%m.%Y"),
                    user=application.applicant if application.applicant else application.created_user,
-                   birthday=application.created_user.birthday.strftime('%d.%m.%Y'),
+                   birthday=application.applicant.birthday.strftime('%d.%m.%Y') if application.applicant else application.created_user.birthday.strftime('%d.%m.%Y'),
                    given_number=car.given_number,
                    old_number=car.old_number,
                    old_technical_passport=car.old_technical_passport,
