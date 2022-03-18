cb2ae8082bbf76e849c42199fdb10c31b69f7d7b
yakubov <yakubov9791999@gmail.com>
2022-03-18 10:34:18 +0500

    changes variables to environment

--- reception/telegram_bot.py
+++ reception/telegram_bot.py
@@ -1,8 +1,11 @@
+import os
 import telegram
 
-BOT_TOKEN = '1546434372:AAEBWg1CQwIJl4-ItzsigwlEor5g4qhDops'   #webdeveloperbot
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")   #webdeveloperbot
 BOT_URL = 'https://api.telegram.org/bot%s' % BOT_TOKEN
-BOT_CHAT_ID = '183551052' #Sirojiddin Yakubov ID
+BOT_CHAT_ID = os.getenv("BOT_CHAT_ID") #Sirojiddin Yakubov ID
 
 def send_message_to_developer(message):
     """
@@ -11,6 +14,6 @@ def send_message_to_developer(message):
     message = 'E-RIB.UZ: ' + message
     bot = telegram.Bot(token=BOT_TOKEN)
     try:
-        bot.sendMessage('183551052', text=message)
+        bot.sendMessage(BOT_CHAT_ID, text=message)
     except:
         pass
